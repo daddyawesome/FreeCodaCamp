35c5da75ba7b00bd56955ec50a8bf5148795c7a5
daddyawesome <sablay296@gmail.com>
2020-01-06 21:21:45 +0000

    Understanding Uninitialized Variables

--- 01_basic.js
+++ 01_basic.js
@@ -21,3 +21,14 @@ Assign the contents of a to variable b.
 */
 a = 7;
 b = a;
+
+//Basic JavaScript: Understanding Uninitialized Variables
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c ="I am a";
+
+// Do not change code below this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
