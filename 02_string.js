2416eb5b7484abde003e11c732a85aa0e66ddd8d
DaddyAwesome <sablay296@gmail.com>
2020-01-13 10:22:03 +0000

    concatenate string using +

---
+++ 02_string.js
@@ -0,0 +1,6 @@
+//Concatenating Strings with Plus Operator
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+var myStr = 'This is the start. ' + 'This is the end.'
